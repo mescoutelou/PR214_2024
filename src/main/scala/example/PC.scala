edbf7c52878924f12369d24a07d312ff89b84a39
OGuillerault <oscar.guillerault@gmail.com>
2024-03-11 17:01:27 +0100

    PC avec mux peu fonctionnel

--- src/main/scala/example/PC.scala
+++ src/main/scala/example/PC.scala
@@ -6,15 +6,29 @@ import prj.common.gen._
 
 class PC extends Module {
     val io = IO(new Bundle{
-        val i_PC_in = Input(UInt(32.W))
-        val i_enable = Input(Bool())
+        val i_count_out = Input(UInt(32.W))
+        val i_donnee = Input(UInt(32.W))
+        val i_enable_mux = Input(Bool())
+        val i_enable_PC = Input(UInt(1.W))
         val o_PC_out = Output(UInt(32.W))
 
     })
 
     val count = 0.U(32.W)
+    val PC_in = 0.U(32.W)
+    
+    PC_in := Mux(io.i_enable_mux, io.i_donnee, io.i_count_out) //Mux avant le PC
 
-    count := Mux(io.i_enable, io.i_PC_in, count + 1.U)
+    when (io.i_enable_PC === 0.U){
+        count := PC_in
+    }
+    .elsewhen(io.i_enable_PC === 1.U){
+        count := count + 1.U
+    }
+    .otherwise{
+        count := 0.U
+    }
+    //count := Mux(io.i_enable_PC, PC_in, count + 1.U) //PC
 
     io.o_PC_out := count 
 }
