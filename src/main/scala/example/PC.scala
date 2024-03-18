e4158d9742f619c2a187b821ad974735749430c4
OGuillerault <oscar.guillerault@gmail.com>
2024-03-18 15:15:12 +0100

    Bloc comprenant PC, Mux et +4 qui compile

--- src/main/scala/example/PC.scala
+++ src/main/scala/example/PC.scala
@@ -6,18 +6,26 @@ import prj.common.gen._
 
 class PC extends Module {
     val io = IO(new Bundle{
-        val i_count_out = Input(UInt(32.W))
         val i_donnee = Input(UInt(32.W))
-        val i_enable_mux = Input(Bool())
+        val i_enable_mux = Input(UInt(1.W))
         val i_enable_PC = Input(UInt(1.W))
         val o_PC_out = Output(UInt(32.W))
 
-    })
+    })  
 
-    val count = 0.U(32.W)
-    val PC_in = 0.U(32.W)
-    
-    PC_in := Mux(io.i_enable_mux, io.i_donnee, io.i_count_out) //Mux avant le PC
+    val count = RegInit(0.U(32.W))
+    val PC_in = RegInit(0.U(32.W))
+    val add_out = RegInit(0.U(32.W))
+    when (io.i_enable_mux === 0.U){
+        PC_in := io.i_donnee
+    }
+    .elsewhen(io.i_enable_mux === 1.U){
+        PC_in := add_out + 1.U
+    }
+    .otherwise{
+        PC_in := DontCare
+    }
+
 
     when (io.i_enable_PC === 0.U){
         count := PC_in
@@ -26,9 +34,10 @@ class PC extends Module {
         count := count + 1.U
     }
     .otherwise{
-        count := 0.U
+        count := DontCare
     }
-    //count := Mux(io.i_enable_PC, PC_in, count + 1.U) //PC
+
+    add_out := count + 4.U
 
     io.o_PC_out := count 
 }
